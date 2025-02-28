df12e85d4856a39529118925d7db53a3188856b7
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-28 16:00:19 +0000

    feat: add input/output examples for user input handling

---
+++ 01_syntax_basics/c_input_output.py
@@ -0,0 +1,17 @@
+# input/output
+# input() function
+# print() function
+
+# The input() function
+# The input() function is used to take input from the user.
+# The input() function always returns a string value.
+
+# Syntax
+# input(prompt)
+
+# Parameters
+# prompt : A string that is written to the console. It is optional.
+
+# Example
+name = input("Enter your name: ") # Enter your name: Ridwan Halim
+print(name)
